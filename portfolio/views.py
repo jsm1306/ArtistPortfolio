97e87c00cfb3bc869a039f370be2b7ca09937220
jsm1306 <sindhu.j1729@gmail.com>
2025-01-22 22:48:14 +0530

    Home Page changes

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import *
+from django.http import HttpResponse
+from django.core.mail import send_mail
+from django.conf import settings
 # Create your views here.
 
 def home(request):
@@ -9,7 +12,17 @@ def blog(request):
     return render(request,'blog.html')
 def shop(request):
     return render(request,'shop.html')
+
+
 def contact(request):
-    return render(request,'contact.html')
+    if request.method == "POST":
+        name = request.POST.get("name")
+        email = request.POST.get("email")
+        message = request.POST.get("message")
+        
+        # For simplicity, just display a success message.
+        return HttpResponse(f"Thank you for your message, {name}!")
+    
+    return render(request, "contact.html")
 def aboutme(request):
     return render(request,'aboutme.html')
